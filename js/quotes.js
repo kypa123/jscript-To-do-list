27d6507cf1a89d9c7e730fb65c5544a16112db42
kypa123 <86966661+kypa123@users.noreply.github.com>
2021-12-31 00:44:42 +0900

    Add files via upload

---
+++ js/quotes.js
@@ -0,0 +1,49 @@
+const quotes=[
+    {
+        quote: "일단 똥을 싸라, 그럼 박수를 쳐줄 것이다.",
+        author:"Andy Asshole",
+    },
+    {
+        quote: "대충 멋있는 말",
+        author:"이말년",
+    },
+    {
+        quote: "흑백배경에 들어가면 뭐든 명언처럼 보인다",
+        author:"주호민",
+    },
+    {
+        quote: "혼란하다 혼란해, 히히!",
+        author:"혼세마왕",
+    },
+    {
+        quote: "습관은 인생을 바꾼다",
+        author:"유튜브 댓글 펌",
+    },
+    {
+        quote: "일단 똥을 싸라, 그럼 박수를 쳐줄 것이다.",
+        author:"Andy Asshole",
+    },
+    {
+        quote: "대충 멋있는 말",
+        author:"이말년",
+    },
+    {
+        quote: "흑백배경에 들어가면 뭐든 명언처럼 보인다",
+        author:"주호민",
+    },
+    {
+        quote: "혼란하다 혼란해, 히히!",
+        author:"혼세마왕",
+    },
+    {
+        quote: "습관은 인생을 바꾼다",
+        author:"유튜브 댓글 펌",
+    },
+];
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
